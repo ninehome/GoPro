61cc66e2fbe257e85dd37b822795f05bb62f8b8e
nine <nine.h@iscmango.com>
2022-10-03 18:29:49 +0800

    update

--- ginblog/routes/router.go
+++ ginblog/routes/router.go
@@ -3,12 +3,19 @@ package routes
 import (
 	"ginblog/utils"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
+	"os"
 )
 
 func InitRouter() *gin.Engine {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
+
+	// 记录到文件。
+	f, _ := os.Create("gin.log")
+	// 如果需要同时将日志写入文件和控制台，请使用以下代码。
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
 	router := r.Group("api/v1")
 
