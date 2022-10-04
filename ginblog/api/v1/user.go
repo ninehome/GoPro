9de48a1384dbede0d20ff97cdd2a4eb8870b8ec4
nine <nine183183@gmail.com>
2022-10-05 01:35:09 +0800

    更新model

--- ginblog/api/v1/user.go
+++ ginblog/api/v1/user.go
@@ -1,6 +1,11 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/model"
+	"ginblog/utils/errormsg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 // 查询用户是否 存在
 func UserExist(ctx *gin.Context) {
@@ -9,7 +14,23 @@ func UserExist(ctx *gin.Context) {
 
 // 添加用户
 func AddUser(ctx *gin.Context) {
+	var data model.User
+	_ = ctx.ShouldBindJSON(&data)
+	code := model.CheckUser(data.Username)
+	if code == errormsg.SUCCESS {
+		model.CreateUser(&data) //写入数据库
+	}
 
+	if code == errormsg.ERROR_USERNAME_USED {
+		code = errormsg.ERROR_USERNAME_USED
+	}
+
+	//返回http json 数据
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errormsg.GetErrorMsg(code),
+	})
 }
 
 //查询单个用户
--- ginblog/model/User.go
+++ ginblog/model/User.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"ginblog/utils/errormsg"
+	"github.com/jinzhu/gorm"
+)
 
 // 登录用户
 type User struct {
@@ -8,5 +11,27 @@ type User struct {
 	Username string `gorm:"type:varchar(20);not null" json:"username"`
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
 	Role     int    `gorm:"type:int" json:"role"` //用户角色
+
+}
+
+// 查询用户是否存在
+func CheckUser(name string) int {
+	var users User
+	//查询 并且绑定到 users ,通过判断users的值 来判断是否查到记录
+	db.Select("id").Where("Username = ? ", name).First(&users)
+	if users.ID > 0 {
+		return errormsg.ERROR_USERNAME_USED //已经存在 不可用
+	}
+	return errormsg.SUCCESS
+}
+
+// 新增/注册用户 返回code
+func CreateUser(data *User) int {
+	//插入记录
+	err := db.Create(&data).Error
+	if err != nil {
+		return errormsg.ERROR //500
+	}
+	return errormsg.SUCCESS //200
 
 }
