fe1c6c117293bd6f9c1c980bf7c98152274783fe
nine <nine.h@iscmango.com>
2022-10-07 19:22:53 +0800

    数据库引入包和驱动要一致,避免出现问题

--- ginblog/model/Upload.go
+++ ginblog/model/Upload.go
@@ -1,1 +1,72 @@
 package model
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+)
+
+// 上传图像接口
+func uploadHandle(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	req.ParseForm()
+	if req.Method != "POST" {
+		w.Write([]byte(html))
+	} else {
+		// 接收图片
+		uploadFile, handle, err := req.FormFile("image")
+		errorHandle(err, w)
+
+		// 检查图片后缀
+		ext := strings.ToLower(path.Ext(handle.Filename))
+		if ext != ".jpg" && ext != ".png" {
+			errorHandle(errors.New("只支持jpg/png图片上传"), w)
+			return
+			//defer os.Exit(2)
+		}
+
+		// 保存图片
+		os.Mkdir("./upload/", os.ModePerm)
+		saveFile, err := os.OpenFile("./uploaded/"+handle.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		errorHandle(err, w)
+		io.Copy(saveFile, uploadFile)
+
+		defer uploadFile.Close()
+		defer saveFile.Close()
+		// 上传图片成功
+		w.Write([]byte("查看上传图片: <a target='_blank' href='/uploaded/" + handle.Filename + "'>" + handle.Filename + "</a>"))
+	}
+}
+
+// 显示图片接口
+func showPicHandle(w http.ResponseWriter, req *http.Request) {
+	file, err := os.Open("." + req.URL.Path)
+	errorHandle(err, w)
+
+	defer file.Close()
+	buff, err := ioutil.ReadAll(file)
+	errorHandle(err, w)
+	w.Write(buff)
+}
+
+// 统一错误输出接口
+func errorHandle(err error, w http.ResponseWriter) {
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+}
+
+const html = `<html>
+    <head></head>
+    <body>
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="image" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>`
