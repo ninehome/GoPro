9ee9b72a8dcba40d28e55c2067e63bdb0d3d0222
nine <nine.h@iscmango.com>
2022-10-12 18:54:50 +0800

    update

---
+++ test/webcli/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import HomeView from "../views/HomeView.vue";
+import Login from '../views/Login.vue'
+import Admin from '../views/Admin.vue'
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+
+  }
+
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  // },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
